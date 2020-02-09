db3fd9a7e0f02d72a3e73099fe16895f40210048
acipiter <shngun@gmail.com>
2020-02-09 11:27:25 +0200

    PopUp Yazar Ekleme

--- Vektorel.Web.KitapBankosuApp/Areas/Admin/Controllers/YazarController.cs
+++ Vektorel.Web.KitapBankosuApp/Areas/Admin/Controllers/YazarController.cs
@@ -9,14 +9,11 @@ namespace Vektorel.Web.KitapBankosuApp.Areas.Admin.Controllers
 {
     public class YazarController : Controller
     {
+        KitapBankosuContext ctx = new KitapBankosuContext();
         // GET: Admin/Yazar
         public ActionResult Index()
         {
-            using (KitapBankosuContext ctx = new KitapBankosuContext())
-            {
-                
-                return View(ctx.Yazarlar.ToList());
-            }
+            return View(ctx.Yazarlar.ToList());
         }
 
         [HttpGet]
@@ -28,23 +25,33 @@ namespace Vektorel.Web.KitapBankosuApp.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult YazarEkle(FormCollection f)
         {
-            using(KitapBankosuContext ctx = new KitapBankosuContext())
-            {
-                Yazar y = new Yazar();
-                y.YazarAd = f["YazarAd"];
-                y.YazarSoyad = f["YazarSoyad"];
-                y.DogumTarih = Convert.ToDateTime(f["DogumTarih"]);
-                ctx.Yazarlar.Add(y);
-                ctx.SaveChanges();
-            }
+            Yazar y = new Yazar();
+            y.YazarAd = f["YazarAd"];
+            y.YazarSoyad = f["YazarSoyad"];
+            y.DogumTarih = Convert.ToDateTime(f["DogumTarih"]);
+            ctx.Yazarlar.Add(y);
+            ctx.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public ActionResult YazarDetay(int? id)
         {
-            using (KitapBankosuContext ctx = new KitapBankosuContext())
+            return View(ctx.Yazarlar.Find(id));
+        }
+
+        public ActionResult YazarSil(int? id)
+        {
+            Yazar yz = ctx.Yazarlar.Find(id);
+            ctx.Yazarlar.Remove(yz);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                return View(ctx.Yazarlar.Find(id));
+                ctx.Dispose();
             }
         }
     }
